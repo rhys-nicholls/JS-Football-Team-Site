37edbf2beff8ef5fe76b74afb0c0d706d571de59
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-03-18 14:25:13 +0000

    Added Layout, Header and Footer Components

---
+++ src/Components/Header_Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TeamLogo } from '../UI/Icons';
+
+const Footer = () => {
+  return (
+    <footer className="bck_blue">
+      <div className="footer_logo">
+        <TeamLogo 
+          width="70px"
+          height="70px"
+          link={true}
+          linkTo="/"
+        />  
+      </div>
+      <div className="footer_discl">
+        Manchester City 2019. All rights reserved
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
