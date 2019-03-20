bb4089329dd092ff61378b826c0578e80e3eab5f
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-03-20 17:00:07 +0000

    Added Feature animation to Home

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
+import { Switch, Route} from 'react-router-dom';
+
+import Home from './Components/Home';
 
 const Routes = (props) => {
   return (
    <Layout>
-     Second Line
+     <Switch>
+      <Route exact component={Home} path="/"/>
+     </Switch>
    </Layout>
   )
 }
