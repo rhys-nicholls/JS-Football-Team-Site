f271b78e6430a8ec2a2444cd6c135587d5675ba6
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-04-01 16:25:41 +0100

    Added Login and Dashboard components

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@ import Layout from './Hoc/Layout';
 import { Switch, Route} from 'react-router-dom';
 
 import Home from './Components/Home';
+import SignIn from './Components/signin';
+
+// Admin routes
+import Dashboard from './Components/admin/Dashboard';
+
 
 const Routes = (props) => {
   return (
    <Layout>
      <Switch>
+     <Route exact component={Dashboard} path="/dashboard"/>
+      <Route exact component={SignIn} path="/sign_in"/>
       <Route exact component={Home} path="/"/>
      </Switch>
    </Layout>
