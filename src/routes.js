4f68f2e92b834e61e6f9b7af5b498b28212fe1e9
rhys-nicholls <34071702+rhys-nicholls@users.noreply.github.com>
2019-04-08 15:20:17 +0100

    Added AddEditMatch component for Admin page

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,14 @@ import SignIn from './Components/signin';
 // Admin routes
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
+import AddEditMatch from './Components/admin/matches/AddEditMatch';
 
 const Routes = (props) => {
   return (
    <Layout>
      <Switch>
+     <PrivateRoute {...props} path="/admin_matches/edit_match/" exact component={AddEditMatch}/>
+     <PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch}/>
       <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
       <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
       <PublicRoute {...props} exact restricted={true} component={SignIn} path="/sign_in"/>
